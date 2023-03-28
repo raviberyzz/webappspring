959fdffd284128b5cab17d4b6cd7fbbd3e2294e1
Uma Maheshwaran <uma.maheshvaran@sunlife.com>
2023-03-28 17:06:46 -0400

    Service Layer: Update

--- src/main/java/ca/sunlife/web/apps/cmsservice/restclient/KafkaClient.java
+++ src/main/java/ca/sunlife/web/apps/cmsservice/restclient/KafkaClient.java
@@ -5,8 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
 import ca.sunlife.web.apps.cmsservice.model.CmsResponse;
 
 @Component
